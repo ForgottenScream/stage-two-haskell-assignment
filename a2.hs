988ef746fd22c8a6ce244523ecd613bbe2df10c9
ForgottenScream <150345575+ForgottenScream@users.noreply.github.com>
2025-03-24 11:23:06 +0000

    current state, basically at the beginning with template modules

---
+++ a2.hs
@@ -0,0 +1,7 @@
+import Base
+import Control.Concurrent
+import DecisionTree
+
+-- TODO: - iterate through [Maybe Player] and display as String in tic tac toe format.
+showBoard :: Board -> String
+showBoard = error "Need to get the array of Maybe Player, interpret and print in tic tac toe format"
