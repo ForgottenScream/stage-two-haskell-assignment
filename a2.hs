03c55cb15db9d34c1ac71f6f824cf0f531ce346e
ForgottenScream <150345575+ForgottenScream@users.noreply.github.com>
2025-03-25 09:22:28 +0000

    potentially working although formatting is off for some reason

--- a2.hs
+++ a2.hs
@@ -2,6 +2,32 @@ import Base
 import Control.Concurrent
 import DecisionTree
 
--- TODO: - iterate through [Maybe Player] and display as String in tic tac toe format.
 showBoard :: Board -> String
-showBoard = error "Need to get the array of Maybe Player, interpret and print in tic tac toe format"
+showBoard board = buildString board "" 0
+  where
+    buildString [] acc _ = acc
+    buildString (x : xs) acc count =
+      let newAcc = acc ++ formatCell x
+          newCount = count + 1
+          accWithNewLine =
+            if newCount `mod` 3 == 0 && not (null xs)
+              then newAcc ++ "\n"
+              else newAcc
+       in buildString xs accWithNewLine newCount
+    formatCell :: Maybe Player -> String
+    formatCell Nothing = "_ "
+    formatCell (Just One) = "X "
+    formatCell (Just Two) = "O "
+
+exampleBoard :: Board
+exampleBoard =
+  [ Just One,
+    Nothing,
+    Just Two,
+    Nothing,
+    Nothing,
+    Nothing,
+    Nothing,
+    Nothing,
+    Just One
+  ]
