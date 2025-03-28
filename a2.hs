d3c4139d56d4ce4969ce0217adfa4f2596d0508d
ForgottenScream <150345575+ForgottenScream@users.noreply.github.com>
2025-03-28 03:46:42 +0000

    okay got a lot better understanding about the first question and assignment overall

--- a2.hs
+++ a2.hs
@@ -3,19 +3,29 @@ import Control.Concurrent
 import DecisionTree
 import Prelude
 
-showBoard :: Board -> IO ()
-showBoard board = putStrLn (unlines [showRow [board !! (i * 3 + j) | j <- [0 .. 2]] | i <- [0 .. 2]])
+exampleBoard =
+  [ Just One,
+    Just Two,
+    Just One,
+    Nothing,
+    Just One,
+    Nothing,
+    Just Two,
+    Nothing,
+    Just Two
+  ]
+
+showBoard :: Board -> String
+showBoard board = unlines [showRow i | i <- [0 .. 2]]
   where
-    showRow :: [Maybe Player] -> String
-    showRow row = unwords [showCell cell | cell <- row]
-    showCell :: Maybe Player -> String
-    showCell Nothing = "_"
-    showCell (Just One) = "X"
-    showCell (Just Two) = "O"
+    showRow i = concat [showCell (board !! (i * 3 + j)) | j <- [0 .. 2]]
+    showCell Nothing = "_ "
+    showCell (Just One) = "X "
+    showCell (Just Two) = "O "
 
 toPos :: (Int, Int) -> Maybe Int
 toPos (x, y)
   | x < 0 || x > 2 || y < 0 || y > 2 = Nothing
-  | otherwise = Just (3 * x + y)
+  | otherwise = Just (x * 3 + y)
 
 -- lookupBoard :: Board -> Int -> Maybe Player
