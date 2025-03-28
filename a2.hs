d06d70696f21f88569e2a0c99da64073e006cd3d
ForgottenScream <150345575+ForgottenScream@users.noreply.github.com>
2025-03-28 10:20:08 +0000

    getting the gameserver going plus quick fix suggestions from vscodium

--- a2.hs
+++ a2.hs
@@ -17,6 +17,21 @@ exampleBoard =
 
 -- Part 1 - Helper Functions
 
+-- Helper Function: Player to String, useful for dialog message
+playerToString :: Player -> String
+playerToString One = "One"
+playerToString Two = "Two"
+
+letterToInt :: Char -> Int
+letterToInt col =
+  case col of
+    'A' -> 0
+    'a' -> 0
+    'B' -> 1
+    'b' -> 1
+    'C' -> 2
+    'c' -> 2
+
 showBoard :: Board -> String
 showBoard board = unlines [showRow i | i <- [0 .. 2]]
   where
@@ -61,17 +76,13 @@ checkWin board =
         case (board !! a, board !! b, board !! c) of
           (Just p1, Just p2, Just p3) | p1 == p2 && p2 == p3 -> Just p1
           _ -> Nothing
-   in foldr
-        ( \combo acc ->
-            case checkLine combo of
-              Just winner -> Just winner
-              Nothing -> acc
-        )
-        Nothing
-        winningCombinations
+  in foldr ( \combo acc -> maybe acc Just (checkLine combo)) Nothing winningCombinations
 
 hline :: IO ()
 hline = putStrLn (replicate 20 '-')
+
+availableMoves :: Board -> Bool
+availableMoves board = Nothing `elem` board
 
 -- Part 2 Game Server and Co-ordination
 select :: Player -> Chan Int -> Chan Int -> Chan Int
@@ -81,3 +92,49 @@ writeChanTwice :: Chan a -> a -> IO ()
 writeChanTwice chan msg = do
   writeChan chan msg
   writeChan chan msg
+
+gameServer ::
+  Player ->
+  Board ->
+  Chan Int ->
+  Chan Int ->
+  Chan Result ->
+  IO (Maybe Player)
+gameServer player boardState p1move p2move resultChan = do
+  hline
+  putStrLn ("Player " ++ playerToString player ++ ", enter your row (0-2) and then column (A-C):")
+
+  rowMove <- readChan (select player p1move p2move)
+  columnMove <- readChan (select player p1move p2move)
+
+  -- Convert the row and column to a position
+  let pos = toPos (rowMove, columnMove)
+
+  case pos of
+    Just index -> do
+      putStrLn ("Player " ++ playerToString player ++ " attempted to move to " ++ show index)
+      case addToGameBoard boardState index player of
+        Just newBoard -> do
+          putStrLn (showBoard newBoard)
+          case checkWin newBoard of
+            Just winner -> do
+              writeChanTwice resultChan Win
+              return (Just winner)
+            Nothing -> do
+              -- Check if there are available moves left
+              if availableMoves newBoard
+                then gameServer (flipPlayer player) newBoard p1move p2move resultChan
+                else do
+                  putStrLn "The game is a draw!"
+                  writeChan resultChan Draw
+                  return Nothing
+        Nothing -> do
+          -- Invalid move: position is already taken
+          putStrLn "Invalid move! This position is already taken. The game continues."
+          writeChanTwice resultChan Continue
+          gameServer (flipPlayer player) boardState p1move p2move resultChan
+    Nothing -> do
+      -- Invalid move: position is out of bounds
+      putStrLn "Invalid position! Please enter a valid row and column. The game continues."
+      writeChanTwice resultChan Continue
+      gameServer (flipPlayer player) boardState p1move p2move resultChan
